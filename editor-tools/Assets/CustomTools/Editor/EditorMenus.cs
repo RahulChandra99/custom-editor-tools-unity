ac0208310b40535f4b0247bde6a3acf86c4abbb7
Rahul Chandra <60950973+RahulChandra99@users.noreply.github.com>
2024-05-27 22:27:54 +0100

    project add

---
+++ editor-tools/Assets/CustomTools/Editor/EditorMenus.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace RC
+{
+    public class EditorMenus
+    {
+        [MenuItem("CustomTools/Project/Project Folder Setup")]
+        public static void IniProjectFolderSetupTool()
+        {
+            ProjectSetup_window.InitWindow();
+        }
+    }
+}
+
+
